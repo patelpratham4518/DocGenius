f7457b4769d0a0692a0df1cff6a33df35f8e449d
Patel Pratham <patelpratham4518@gmail.com>
2024-05-28 18:45:08 +0530

    get webViewLink from Id else open popup

--- force-app/main/default/lwc/docsTemplate/docsTemplate.js
+++ force-app/main/default/lwc/docsTemplate/docsTemplate.js
@@ -1,27 +1,45 @@
 import { LightningElement, track, wire ,api} from 'lwc';
 import getAllDocs from '@salesforce/apex/GoogleDriveTemplatesController.getAllDocs'
+import getTemplate from '@salesforce/apex/GoogleDriveTemplatesController.getTemplate'
 
 export default class DocsTemplate extends LightningElement {
 
-    @api templateId
+    @api templateId = "a09F300000DMIOmIAP"
     @api objectName
 
     isSpinner = true
     selectedTemplate
-    showPopup
+    showPopup = false
+    webViewLink
     @track templates
    
     connectedCallback(){
-        if (!this.selectedTemplate) {
-            this.showPopup = true
-            getAllDocs({isAccessError : false, newAccessToken : null})
-            .then((response)=>{
-                 this.templates =  JSON.parse(response)
-                    this.isSpinner = false
-            })
-        }else {
-            this.showPopup = false
-        }
+
+        getTemplate({id:this.templateId}).then(response => {
+            console.log('Response ==> ',response);
+            if (response) {
+                this.webViewLink = response
+                this.isSpinner = false
+            } else {
+                this.showPopup = true
+                getAllDocs({isAccessError : false, newAccessToken : null})
+                .then((response)=>{
+                     this.templates =  JSON.parse(response)
+                        this.isSpinner = false
+                })
+            }
+        }).catch(error => {
+            console.log('Error ==> ',error);
+        })
+
+        // if (!this.selectedTemplate) {
+        //     this.showPopup = true
+        //     getAllDocs({isAccessError : false, newAccessToken : null})
+        //     .then((response)=>{
+        //          this.templates =  JSON.parse(response)
+        //             this.isSpinner = false
+        //     })
+        // }
     }
     closePopup(){
         this.showPopup = false
@@ -32,7 +50,7 @@ export default class DocsTemplate extends LightningElement {
     handleTemplateClick(event) {
         const templateId = event.target.dataset.id;
         this.selectedTemplate = this.templates.find(template => template.id === templateId);
-        console.log('Selected template ==> '+ this.selectedTemplate);
+        this.webViewLink = this.selectedTemplate.webViewLink
         this.closePopup(); 
     }
 
