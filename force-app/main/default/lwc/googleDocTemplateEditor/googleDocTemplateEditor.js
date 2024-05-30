3f5224807be0c219ba1fd93f23a9bd8a632a1cf6
Patel Pratham <patelpratham4518@gmail.com>
2024-05-30 17:53:36 +0530

    All docs showable

--- force-app/main/default/lwc/googleDocTemplateEditor/googleDocTemplateEditor.js
+++ force-app/main/default/lwc/googleDocTemplateEditor/googleDocTemplateEditor.js
@@ -22,8 +22,9 @@ export default class GoogleDocTemplateEditor extends LightningElement {
                 this.showPopup = true
                 getAllDocs()
                 .then((response)=>{
-                     this.templates =  JSON.parse(response)
-                        this.isSpinner = false
+                    this.templates =  JSON.parse(response)
+                    this.setDateAndSize()
+                    this.isSpinner = false
                 }).catch(error => {
                     console.log('Error ==> ',error);
                 })
@@ -48,7 +49,8 @@ export default class GoogleDocTemplateEditor extends LightningElement {
         getAllDocs()
         .then((response)=>{
              this.templates =  JSON.parse(response)
-                this.isSpinner = false
+             this.setDateAndSize()
+            this.isSpinner = false
         }).catch(error => {
             console.log('Error ==> ',error);
         })
@@ -59,5 +61,19 @@ export default class GoogleDocTemplateEditor extends LightningElement {
             this.webViewLink = this.selectedTemplate.webViewLink
             this.closePopup()
         }
+    }
+
+    setDateAndSize(){
+        this.templates = this.templates.map(template => {
+            template.createdTime = template.createdTime.split("T")[0]
+            if (template.size < 1024) {
+                template.size = Math.round(template.size)+"Byte"
+            } else if (template.size < 1024*1024) {
+                template.size = Math.round(template.size/1024)+"KB"
+            }else{
+                template.size = Math.round(template.size/(1024*1024))+"MB"
+            }
+            return template
+        })
     }
 }
